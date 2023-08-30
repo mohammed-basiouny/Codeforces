4c258b548c7d21c1890b26961b2093736f8fb6f0
Mohammed Basyouny <basiony.dev@gmail.com>
2023-08-31 00:47:13 +0200

    Simple Calculator

---
+++ 01.Sheet #1/SimpleCalculator.cpp
@@ -0,0 +1,13 @@
+/*Given two numbers X and Y. Print the summation and multiplication and subtraction of these 2 numbers.*/
+
+
+#include <iostream>
+using namespace std;
+int main() {
+
+    long long num1,num2;
+    cin >> num1>>num2;
+    cout <<num1<<" + "<<num2<<" = "<<num1+num2<<endl;
+    cout <<num1<<" * "<<num2<<" = "<<num1*num2<<endl;
+    cout <<num1<<" - "<<num2<<" = "<<num1-num2<<endl;
+}
